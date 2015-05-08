59c63d52b9857d4fa4d9867fc289b057337061f1
Bryan Novak <Bryan125@live.missouristate.edu>
2015-05-08 07:40:45 -0500

    add more files

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      'dist-server': {
+        options: {
+          port: 8000,
+          keepalive: true,
+          base: 'dist',
+          middleware: function (connect, options) {
+            return [
+              // Serve static files.
+              connect.static(options.base[0]),
+              // Make empty directories browsable.
+              connect.directory(options.base[0])
+            ];
+          }
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'build/app.js': [
+            'js/controllers.js',
+            'js/router.js'
+          ]
+        }
+      }
+    },
+    concat: {
+      dist: {
+        files: {
+          'dist/app.js': [
+            'node_modules/angular/angular.min.js',
+            'node_modules/angular-route/angular-route.min.js',
+            'build/app.js'
+          ]
+        }
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          'build/app.css': [
+            'css/style.css'
+          ]
+        }
+      }
+    },
+    copy: {
+      dist: {
+        files: [
+          {expand: true, src: ['partials/*'], dest: 'dist/'},
+          {expand: true, src: ['index.html'], dest: 'dist/'},
+          {expand: true, src: ['app.css'], dest: 'dist/', cwd: 'build'}
+        ],
+      }
+    },
+    processhtml: {
+      dist: {
+        files: [
+          {expand: true, src: ['index.html'], dest: 'dist/'}
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('dist-test', ['dist', 'connect:dist-server']);
+  grunt.registerTask('dist', ['uglify:dist', 'cssmin:dist', 'concat:dist', 'copy:dist', 'processhtml:dist']);
+};
